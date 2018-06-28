e0ef5611122b171c7c6e7f5c7118a7b50bcd89f9
lonycell <lonycell@gmail.com>
2018-06-28 18:20:59 +0900

    dist

--- dist/panel/cpktrend-table/module.js
+++ dist/panel/cpktrend-table/module.js
@@ -29324,7 +29324,7 @@ var RmsCPKTrendPanelCtrl = /** @class */ (function (_super) {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"editor-row\">\n    <div class=\"thingspin-table\"></div>\n</div>\n";
+module.exports = "<div class=\"editor-row\">\r\n    <div class=\"thingspin-table\"></div>\r\n</div>\r\n";
 
 /***/ }),
 
