86732e838205bbb00f8717f20667bdd58b7e17eb
jiwoong Park <jiwoong@mdstec.com>
2018-07-09 12:45:47 +0900

    CPK Trend Table 에 GUI 적용

--- src/panel/cpktrend-table/module.ts
+++ src/panel/cpktrend-table/module.ts
@@ -73,6 +73,14 @@ class RmsCPKTrendPanelCtrl extends MetricsPanelCtrl {
     t.id = this.divID;
 
     this.container = $(t);
+
+    var link = document.createElement( "link" );
+    link.href = "public/plugins/proj-rms-plugin-app/panel/mold-list/css/light.css";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+
+    document.getElementsByTagName( "head" )[0].appendChild( link );
   }
 
   /* dynamic ui process
